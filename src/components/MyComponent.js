afbff97242453bbf2e74e8d8d525ed2456800547
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2022-08-10 14:47:45 -0300

    css

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,7 +1,10 @@
+import "./MyComponent.css";
+
 const MyComponent = () =>{
     return(
         <div>
             <h3>Estou sendo importado!</h3>
+            <p>Teste</p>
         </div>
     );
 };
