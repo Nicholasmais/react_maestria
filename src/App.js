b63599d09eff5a0d27b520c034d89365405a7217
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2022-09-05 14:28:08 -0300

    delete product

--- src/App.js
+++ src/App.js
@@ -13,21 +13,20 @@ function App() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [pessoa,setPessoa] = useState("Desconhecido");
-  const url = "http://172.17.25.44:3000/products"
-  
-  useEffect(() => {
-    async function fecthData(){
-      const response = await fetch(url);
-      const data = await response.json();
-      setProducts(data);
-    }
-   fecthData();
-  }, []);
+  const [loading, setLoading] = useState(false);
+  const [id, setId] = useState();
 
+  const url = "http://192.168.0.7:3000/products"
+  
+  const fecthData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setProducts(data);
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+    setLoading(true);  
     const product = {
       name,
       price,
@@ -41,10 +40,25 @@ function App() {
       body:JSON.stringify(product)
     });
 
-  const addedProduct = await response.json();
-  setProducts((prevProducts) => [...prevProducts, addedProduct])
+    const addedProduct = await response.json();
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+    setLoading(false);
+    };
 
-  };
+  const deleteProduct = async (e) => {
+    e.preventDefault();
+    await fetch(url + `/${id}`, {
+      method:"DELETE",
+      headers:{
+        "Content-type":"application/json"
+    }
+  }).then((response) => response.json());
+  fecthData();
+  }
+
+  useEffect(() => {
+    fecthData();
+   }, []);
 
   return (
     <>
@@ -53,14 +67,15 @@ function App() {
         <span>Qual seu nome?</span>
         <input type="text" onChange={(e) => {setPessoa(e.target.value)}} />
       </label>
-      <div>
+      <div >
         <h2>Lista de produtos</h2>
-        <ul>
+        {loading ? <div>Carreagando</div> :
+        <ul className='products'>
           {products.map((produto) => {
-            return <li key={produto.id}>{produto.name} - R${produto.price}</li>
+            return (<><li key={produto.id}>{produto.name} - R${produto.price}</li>  <div style={{'font-size':'10px','marginTop':'0%', marginBottom:'0.5em'}}>Código = {produto.id}</div></>)
           })}
         </ul>
-
+      }
 
           <div className="add-product">
             <form action="" onSubmit={handleSubmit}>
@@ -74,7 +89,15 @@ function App() {
               <input type="number" value={price} name={price} onChange={(e)=>setPrice(e.target.value)} />
             </label>
             
-            <input type="submit" value="Criar"/>
+            <input type="submit" value="Criar" disabled={loading}/>
+            </form>
+
+            <form action="" onSubmit={deleteProduct}>
+              <label htmlFor="">Id do produto a ser deletado: 
+                <input type="number" name="" id="" onChange={(e) => {setId(e.target.value)}}/>
+                <input type="submit" value="Deletar" disabled={loading} />
+              </label>
+
             </form>
           </div>
 
